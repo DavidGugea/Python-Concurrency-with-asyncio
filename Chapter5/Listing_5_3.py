c35ded70aaf67ef1b3157146169697733a774b60
David Gugea <davidgugea@yahoo.com>
2022-08-30 09:07:11 +0200

    Finished Chapter 5

---
+++ Chapter5/Listing_5_3.py
@@ -0,0 +1,37 @@
+import asyncio
+
+import asyncpg
+
+from Listing_5_2 import *
+
+
+async def main():
+    connection = await asyncpg.connect(
+        host='127.0.0.1',
+        port=5432,
+        user='postgres',
+        database='postgres',
+        password='password'
+    )
+
+    statements = [
+        CREATE_BRAND_TABLE,
+        CREATE_PRODUCT_TABLE,
+        CREATE_PRODUCT_COLOR_TABLE,
+        CREATE_PRODUCT_SIZE_TABLE,
+        CREATE_SKU_TABLE,
+        SIZE_INSERT,
+        COLOR_INSERT
+    ]
+
+    print('Creating the product database...')
+
+    for statement in statements:
+        status = await connection.execute(statement)
+        print(status)
+
+    print('Finished creating the product database!')
+    await connection.close()
+
+
+asyncio.run(main())
